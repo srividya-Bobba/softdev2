d481bceb151cf244fbd81ece18c4bce032928f51
srividya-Bobba <srividyabobba9@gmail.com>
2022-10-10 12:09:32 +0100

    java script

---
+++ script1.js
@@ -0,0 +1,10 @@
+
+
+      var name = "srividya";  // string type
+        var age = 23;  //Number type
+        var student = true; // Boolean type: Notice all lowercase true
+        console.log(name);
+        document.write('Name ' + name + '<br>');  // + is used for string concatentation
+        document.write('Age ' + age + '<br>');  // Try using + with another number and see what happens!
+        document.write('Is student? ' + student);    //
+         
---
+++ script2.js
@@ -0,0 +1,6 @@
+function msg() {
+    document.write("You clicked the button!");
+}
+function change() {
+    document.getElementById("my_text").innerHTML = "You clicked me";
+}
